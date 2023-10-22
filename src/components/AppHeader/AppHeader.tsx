e9ecf41379dea3b7b88a23061f653a198260b540
theburntcrumpet <msksansom@gmail.com>
2023-10-22 19:45:49 +0100

    Add dark mode toggle

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,8 +1,14 @@
-import { Heading, Text } from "@chakra-ui/react"
+import { MoonIcon, SunIcon } from "@chakra-ui/icons"
+import { Button, Heading, Text, Tooltip, useColorMode } from "@chakra-ui/react"
 
 function AppHeader() {
-     return (<>
-        <Heading>jwty</Heading>
+    const { colorMode, toggleColorMode } = useColorMode()
+    return (<>
+        <Heading>jwty&nbsp;
+            <Tooltip label={colorMode === "light" ? "Switch to Dark Mode" : "Switch to Light Mode"}>
+                {colorMode === "light" ? <MoonIcon onClick={toggleColorMode} /> : <SunIcon onClick={toggleColorMode} />}
+            </Tooltip>
+        </Heading>
         <Text fontSize='xl'>be a bit jwty</Text>
     </>);
 }
